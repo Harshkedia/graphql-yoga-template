09a7b8ae35ca5e410a23528a0809ef45445dc17a
Harsh Kedia <harshkedi@gmail.com>
2020-02-26 10:10:54 -0800

    changed server.js to index.js

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ const server = new GraphQLServer({
   typeDefs: "./schema.graphql",
   resolvers
 });
-server.start(() => console.log("Server is running on localhost:4000"));
+server.start(() => console.log("Server is running on http://localhost:4000"));
